430ee5719a21ce7935336294120c5802b4d5c05f
Edison Neto <ednetoali@gmail.com>
2020-07-19 19:57:17 -0300

    Add remove first, remove last and search functions

--- linked_list/linked_list.go
+++ linked_list/linked_list.go
@@ -11,7 +11,7 @@ type LinkedList struct {
 	tail *Node
 }
 
-func (list *LinkedList) Add(element int) error {
+func (list *LinkedList) Add(element int) {
 	if list.head == nil {
 		list.head = &Node{
 			next:  nil,
@@ -19,25 +19,57 @@ func (list *LinkedList) Add(element int) error {
 			value: element,
 		}
 		list.tail = list.head
-		return nil
+	} else {
+		new_node := &Node{
+			prev:  list.tail,
+			next:  nil,
+			value: element,
+		}
+
+		list.tail.next = new_node
+		list.tail = new_node
 	}
-
-	new_node := &Node{
-		prev:  list.tail,
-		next:  nil,
-		value: element,
-	}
-
-	list.tail.next = new_node
-	list.tail = new_node
-
-	return nil
 }
 
-func (list *LinkedList) Search(element int) error {
-	return nil
+func (list *LinkedList) Search(element int) bool {
+	if list.head == nil {
+		return false
+	}
+
+	nodeHead := list.head
+	nodeTail := list.tail
+	for nodeHead.prev != nodeTail {
+		if nodeHead.value == element || nodeTail.value == element {
+			return true
+		}
+		nodeHead = nodeHead.next
+		nodeTail = nodeTail.prev
+	}
+	return false
 }
 
 func (list *LinkedList) RemoveLast() error {
+	if list.head == nil {
+		return nil
+	} else if list.head == list.tail {
+		list.head = nil
+		list.tail = nil
+	} else {
+		list.tail.prev.next = nil
+		list.tail = list.tail.prev
+	}
+	return nil
+}
+
+func (list *LinkedList) RemoveFirst() error {
+	if list.head == nil {
+		return nil
+	} else if list.head == list.tail {
+		list.head = nil
+		list.tail = nil
+	} else {
+		list.head.next.prev = nil
+		list.head = list.head.next
+	}
 	return nil
 }
--- linked_list/linked_list_test.go
+++ linked_list/linked_list_test.go
@@ -10,9 +10,8 @@ func Test_add_element_to_empty_list_should_create_head_and_tail_with_value(t *te
 	final_list := []int{2}
 	linked_list := LinkedList{nil, nil}
 
-	error := linked_list.Add(2)
+	linked_list.Add(2)
 
-	assert.Nil(t, error)
 	assert.Equal(t, 2, linked_list.head.value)
 	assert.Equal(t, 2, linked_list.tail.value)
 	assert.Nil(t, linked_list.head.prev)
@@ -26,9 +25,8 @@ func Test_add_element_to_list_with_one_element_should_have_different_head_and_ta
 	linked_list := LinkedList{nil, nil}
 	linked_list.Add(2)
 
-	error := linked_list.Add(3)
+	linked_list.Add(3)
 
-	assert.Nil(t, error)
 	assert.Nil(t, linked_list.head.prev)
 	assert.Nil(t, linked_list.tail.next)
 	assert.Equal(t, linked_list.head.next, linked_list.tail)
@@ -43,9 +41,8 @@ func Test_add_element_to_list_with_two_elements_list_should_have_three_elements(
 	linked_list.Add(2)
 	linked_list.Add(3)
 
-	error := linked_list.Add(4)
+	linked_list.Add(4)
 
-	assert.Nil(t, error)
 	assert.Nil(t, linked_list.head.prev)
 	assert.Nil(t, linked_list.tail.next)
 	assert.NotEqual(t, linked_list.head.next, linked_list.tail)
@@ -63,15 +60,177 @@ func Test_add_element_to_list_with_four_elements_list_should_have_five_elements(
 	linked_list.Add(4)
 	linked_list.Add(5)
 
-	error := linked_list.Add(6)
+	linked_list.Add(6)
 
-	assert.Nil(t, error)
 	assert.Nil(t, linked_list.head.prev)
 	assert.Nil(t, linked_list.tail.next)
 	assert.NotEqual(t, linked_list.head.next, linked_list.tail)
 	assert.NotEqual(t, linked_list.tail.prev, linked_list.head)
 
 	assertList(t, &linked_list, final_list)
+}
+
+func Test_search_first_element_in_list_should_return_true(t *testing.T) {
+	linked_list := LinkedList{nil, nil}
+	linked_list.Add(2)
+	linked_list.Add(3)
+	linked_list.Add(4)
+	linked_list.Add(5)
+
+	found := linked_list.Search(2)
+
+	assert.True(t, found)
+}
+
+func Test_search_last_element_in_list_should_return_true(t *testing.T) {
+	linked_list := LinkedList{nil, nil}
+	linked_list.Add(2)
+	linked_list.Add(3)
+	linked_list.Add(4)
+	linked_list.Add(5)
+
+	found := linked_list.Search(5)
+
+	assert.True(t, found)
+}
+
+func Test_search_element_in_middle_of_odd_list_should_return_true(t *testing.T) {
+	linked_list := LinkedList{nil, nil}
+	linked_list.Add(2)
+	linked_list.Add(3)
+	linked_list.Add(4)
+	linked_list.Add(5)
+	linked_list.Add(6)
+
+	found := linked_list.Search(4)
+
+	assert.True(t, found)
+}
+
+func Test_search_element_in_first_middle_part_of_even_list_should_return_true(t *testing.T) {
+	linked_list := LinkedList{nil, nil}
+	linked_list.Add(2)
+	linked_list.Add(3)
+	linked_list.Add(4)
+	linked_list.Add(5)
+	linked_list.Add(6)
+	linked_list.Add(7)
+
+	found := linked_list.Search(4)
+
+	assert.True(t, found)
+}
+
+func Test_search_element_in_second_middle_part_of_even_list_should_return_true(t *testing.T) {
+	linked_list := LinkedList{nil, nil}
+	linked_list.Add(2)
+	linked_list.Add(3)
+	linked_list.Add(4)
+	linked_list.Add(5)
+	linked_list.Add(6)
+	linked_list.Add(7)
+
+	found := linked_list.Search(5)
+
+	assert.True(t, found)
+}
+
+func Test_search_for_element_not_on_list_should_return_false(t *testing.T) {
+	linked_list := LinkedList{nil, nil}
+	linked_list.Add(2)
+	linked_list.Add(3)
+	linked_list.Add(4)
+	linked_list.Add(5)
+	linked_list.Add(6)
+	linked_list.Add(7)
+
+	found := linked_list.Search(15)
+
+	assert.False(t, found)
+}
+
+func Test_search_for_element_in_empty_list_should_return_false(t *testing.T) {
+	linked_list := LinkedList{nil, nil}
+
+	found := linked_list.Search(1)
+
+	assert.False(t, found)
+}
+
+func Test_removing_last_element_from_list_with_multiple_elements_should_remove_last_element(t *testing.T) {
+	final_list := []int{2, 3, 4, 5, 6}
+	linked_list := LinkedList{nil, nil}
+	linked_list.Add(2)
+	linked_list.Add(3)
+	linked_list.Add(4)
+	linked_list.Add(5)
+	linked_list.Add(6)
+	linked_list.Add(7)
+
+	error := linked_list.RemoveLast()
+
+	assert.Nil(t, error)
+
+	assertList(t, &linked_list, final_list)
+}
+
+func Test_removing_last_element_from_empty_list_should_keep_list_empty(t *testing.T) {
+	linked_list := LinkedList{nil, nil}
+
+	error := linked_list.RemoveLast()
+
+	assert.Nil(t, error)
+	assert.Nil(t, linked_list.head)
+	assert.Nil(t, linked_list.tail)
+}
+
+func Test_removing_last_element_from_list_with_one_element_should_make_list_empty(t *testing.T) {
+	linked_list := LinkedList{nil, nil}
+	linked_list.Add(2)
+
+	error := linked_list.RemoveLast()
+
+	assert.Nil(t, error)
+	assert.Nil(t, linked_list.head)
+	assert.Nil(t, linked_list.tail)
+}
+
+func Test_removing_first_element_from_list_with_multiple_elements_should_remove_first_element(t *testing.T) {
+	final_list := []int{3, 4, 5, 6, 7}
+	linked_list := LinkedList{nil, nil}
+	linked_list.Add(2)
+	linked_list.Add(3)
+	linked_list.Add(4)
+	linked_list.Add(5)
+	linked_list.Add(6)
+	linked_list.Add(7)
+
+	error := linked_list.RemoveFirst()
+
+	assert.Nil(t, error)
+
+	assertList(t, &linked_list, final_list)
+}
+
+func Test_removing_first_element_from_empty_list_should_keep_list_empty(t *testing.T) {
+	linked_list := LinkedList{nil, nil}
+
+	error := linked_list.RemoveFirst()
+
+	assert.Nil(t, error)
+	assert.Nil(t, linked_list.head)
+	assert.Nil(t, linked_list.tail)
+}
+
+func Test_removing_first_element_from_list_with_one_element_should_make_list_empty(t *testing.T) {
+	linked_list := LinkedList{nil, nil}
+	linked_list.Add(2)
+
+	error := linked_list.RemoveFirst()
+
+	assert.Nil(t, error)
+	assert.Nil(t, linked_list.head)
+	assert.Nil(t, linked_list.tail)
 }
 
 func assertList(t *testing.T, list *LinkedList, validation_list []int) {
